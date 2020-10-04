3a28e4d95714173dfb14d6391d0498caf3644781
iagokrt <iago_floripa@hotmai.com>
2020-10-04 20:34:02 -0300

    api axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
-
-const api = axios.create({
-  // ny times access to api
-  baseURL: 'https://api.github.com',
-});
-
-export default api;
+async function getUser() {
+  try {
+    const response = await axios.get('/user?ID=12345');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
