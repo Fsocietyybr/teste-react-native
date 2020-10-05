268c8624448e3c46d680053f499b53020fb9b938
iagokrt <iago_floripa@hotmai.com>
2020-10-04 22:11:50 -0300

    api connection

--- App.js
+++ App.js
@@ -7,13 +7,23 @@ import api from './src/services/api'
 export default function App() {
   const [info, setInfo] = useState([]);
 
+  // Load dos dados da api
+  useEffect(() => {
+    async function loadNews() {
+      const response = await api.get('/arts.json');
+      console.log(response.data);
+      setInfo(response.data);
+    }
+
+    loadNews();
+  }, []);
+
   return (
     <>
       <Main>
         <Title>T1M3 2 RE4D</Title>
         <Title>._Technology</Title>
         <Title>Science</Title>
-
       </Main>
     </>
   );
